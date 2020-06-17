39a111b636a093c675a24ecb4424e4966718a6c2
pangpuncake <58969383+pangpuncake@users.noreply.github.com>
2020-06-17 20:19:32 +0800

    Update App.js
    Merge Conflict fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import './App.css';
-<<<<<<< HEAD
 import { Route, Switch } from 'react-router-dom';
 // import CodeMirrorEditor from './components/CodeMirrorEditor/CodeMirrorEditor';
 import Forum from '../src/components/Forum/Forum'
-=======
 import MainText from './components/MainText/MainText';
->>>>>>> 384ac102ec8b667505f08a487a80d9777309b76a
 
 class App extends Component {
   render() {
