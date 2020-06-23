062bfecfdafde1090c16c6a2a52fa3509b3f9562
lohyingxin <65619666+lohyingxin@users.noreply.github.com>
2020-06-24 00:08:04 +0800

    styled forum and main page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 // import CodeMirrorEditor from './components/CodeMirrorEditor/CodeMirrorEditor';
-import Forum from '../src/components/Forum/Forum'
-import MainText from './components/MainText/MainText';
+import Forum from "../src/components/Forum/Forum";
+import MainText from "./components/MainText/MainText";
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <Switch>
-            {/* <Route path="/code" component={Checkout} />
+          {/* <Route path="/code" component={Checkout} />
             <Route path="/algo" component={Orders} /> */}
-            <Route path="/forum" component={Forum} />
-            <Route path="/" exact component={MainText} />
-          </Switch>
+          <Route path='/forum' component={Forum} />
+          <Route path='/' exact component={MainText} />
+        </Switch>
       </Layout>
     );
   }
--- src/components/Forum/Sidebar/Sidebar.js
+++ src/components/Forum/Sidebar/Sidebar.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import NavigationItem from '../../Layout/Navigation/NavigationItems/NavigationItem/NavigationItem'
+import React from "react";
+import NavigationItem from "../../Layout/Navigation/NavigationItems/NavigationItem/NavigationItem";
 
 const Sidebar = () => {
-    return (
-        <div style = {{backgroundColor: 'slategrey', width: '20%'}}>
-            <ul>
-                <NavigationItem link = "/forum/home">Home</NavigationItem>
-                <NavigationItem link = "/forum">Create Post</NavigationItem>
-                <NavigationItem link = "/forum/tags">Tags</NavigationItem>
-                <NavigationItem link = "/forum/popular">Popular</NavigationItem>
-                <NavigationItem link = "/forum/latest">Latest</NavigationItem>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        backgroundColor: "slategrey",
+        width: "20%",
+        height: "100vh",
+      }}
+    >
+      <ul style={{ overflow: "hidden", position: "fixed", padding: "150px 0" }}>
+        <NavigationItem link='/forum/home'>Home</NavigationItem>
+        <NavigationItem link='/forum'>Create Post</NavigationItem>
+        <NavigationItem link='/forum/tags'>Tags</NavigationItem>
+        <NavigationItem link='/forum/popular'>Popular</NavigationItem>
+        <NavigationItem link='/forum/latest'>Latest</NavigationItem>
+      </ul>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
